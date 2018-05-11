736d055092852feb742365de03a66746ca8717d7
伊泰信息 <@192.168.196.1>
2018-05-11 10:49:30 +0800

    岗位页面组织树

--- WebContent/common/js/system/position/positionListPage.js
+++ WebContent/common/js/system/position/positionListPage.js
@@ -2,6 +2,8 @@
  * 岗位管理JS
  */
 
+var zNodes ='';
+
 var setting = {
 			check: {
 				enable: true,
@@ -17,32 +19,16 @@ var setting = {
 				}				
 			}
 		};
-var zNodes =[
-	{ id:1, pId:0, name:"随意勾选 1", open:true},
-	{ id:11, pId:1, name:"随意勾选 1-1", open:true},
-	{ id:111, pId:11, name:"随意勾选 1-1-1"},
-	{ id:112, pId:11, name:"随意勾选 1-1-2"},
-	{ id:12, pId:1, name:"随意勾选 1-2", open:true},
-	{ id:121, pId:12, name:"随意勾选 1-2-1"},
-	{ id:122, pId:12, name:"随意勾选 1-2-2"},
-	{ id:2, pId:0, name:"随意勾选 2", checked:true, open:true},
-	{ id:21, pId:2, name:"随意勾选 2-1"},
-	{ id:22, pId:2, name:"随意勾选 2-2", open:true},
-	{ id:221, pId:22, name:"随意勾选 2-2-1", checked:true},
-	{ id:222, pId:22, name:"随意勾选 2-2-2"},
-	{ id:23, pId:2, name:"随意勾选 2-3"}
-];
 
-var code;
-
-
-function showCode(str) {
-	if (!code) code = $("#code");
-	code.empty();
-	code.append("<li>"+str+"</li>");
-}
 
 $(document).ready(function(){
-	$.fn.zTree.init($("#positionTree"), setting, zNodes);
+	$.get(ctx+"org/organizations",function(data){ 
+		if(data.res =="2"){ 
+			zNodes = (data.list) ; 					
+			$.fn.zTree.init($("#positionTree"), setting, zNodes);
+			
+		}
+	});
+	
 });
 		
