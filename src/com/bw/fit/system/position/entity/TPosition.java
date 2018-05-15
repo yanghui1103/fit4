70060f2aef610af324e227857304d9aa814fe22d
伊泰信息 <@DESKTOP-GULQMNK>
2018-05-15 14:47:59 +0800

    岗位实体

---
+++ src/com/bw/fit/system/position/entity/TPosition.java
@@ -0,0 +1,28 @@
+package com.bw.fit.system.position.entity;
+
+import com.bw.fit.system.common.entity.BaseEntity;
+
+public class TPosition extends BaseEntity{
+	private String name;
+	private String code ;
+	private String simpleName;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getSimpleName() {
+		return simpleName;
+	}
+	public void setSimpleName(String simpleName) {
+		this.simpleName = simpleName;
+	}
+	
+}
