a167b04aebf5b125e69a92bbe07319d7e2607d92
yangh <yanghui1103@foxmail.com>
2018-05-21 17:40:38 +0800

    账号删除

--- src/com/bw/fit/system/account/controller/AccountController.java
+++ src/com/bw/fit/system/account/controller/AccountController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,7 @@ import com.bw.fit.system.account.dao.AccountDao;
 import com.bw.fit.system.account.model.Account;
 import com.bw.fit.system.account.service.AccountService;
 import com.bw.fit.system.common.controller.BaseController;
+import com.bw.fit.system.common.model.RbackException;
 import com.bw.fit.system.common.util.PubFun;
 import com.bw.fit.system.menu.model.Menu;
 import com.bw.fit.system.menu.service.MenuService;
@@ -61,4 +63,21 @@ public class AccountController extends BaseController {
 		js.put("rows",  JSONObject.toJSON(list));
 		return js ;
 	}
+	
+	@RequestMapping(value="account/{id}",method=RequestMethod.DELETE,produces="application/json;charset=UTF-8")
+	@ResponseBody
+	public JSONObject delete(@PathVariable String id){
+		JSONObject json = new JSONObject();		
+		try {
+			json = accountService.delete(id);
+		} catch (RbackException e) {
+			e.printStackTrace();
+			json = new JSONObject();		
+			PubFun.returnFailJson(json, e.getMsg());
+			throw e;
+		}finally{
+			return json ;
+		}
+	}
+	
 }
