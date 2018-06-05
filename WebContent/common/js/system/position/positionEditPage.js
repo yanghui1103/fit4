2915e5b8262f2d958b495766c37536559cf7244c
伊泰信息 <@DESKTOP-GULQMNK>
2018-06-05 09:13:14 +0800

    组织修改地址本只显示组织

--- WebContent/common/js/system/position/positionEditPage.js
+++ WebContent/common/js/system/position/positionEditPage.js
@@ -6,7 +6,7 @@ $(function(){
 			icons: [{
 				iconCls:'icon-search',
 				handler: function(e){
-					openAddress($("#_loadDialog_address"),$("input[name='temp_str1']"),$(".address-select"),'OPA',true);
+					openAddress($("#_loadDialog_address"),$("input[name='temp_str1']"),$(".address-select"),'O',true);
 				}
 			}]
 		})
